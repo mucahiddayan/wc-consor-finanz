50fbf6c5d6bdf3705a17feacdb628dfd72c372cb
Mücahid Dayan <muecahid.dayan@westernacher.com>
2020-03-15 11:23:02 +0100

    create plugin file

---
+++ wc-consor-finanz.php
@@ -0,0 +1,19 @@
+/**
+ * Plugin Name: WooCommerce Consors Finanz Extension
+ * Plugin URI: https://dayan.one
+ * Description: Consors Finanz  Plugin for WooCommerce.
+ * Version: 1.0.0
+ * Author: Mücahid Dayan
+ * Author URI: https://mücahiddayan.com/
+ * Developer: Mücahid Dayan
+ * Developer URI: https://mücahiddayan.com/
+ * Text Domain: wc-consor-finanz
+ * Domain Path: /languages
+ *
+ * Woo: 12345:342928dfsfhsf8429842374wdf4234sfd
+ * WC requires at least: 2.2
+ * WC tested up to: 2.3
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
